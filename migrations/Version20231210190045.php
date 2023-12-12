229146c1a294200dbe0f01c52dc8f1fe95af7c79
roberto <robertoramirezmoreno@gmail.com>
2023-12-12 06:25:50 +0000

    AA

--- migrations/Version20231210190045.php
+++ migrations/Version20231210190045.php
@@ -31,6 +31,8 @@ final class Version20231210190045 extends AbstractMigration
             roles JSON NOT NULL, 
             PRIMARY KEY(id)
         ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+
+$this->addSql('ALTER TABLE user ADD email VARCHAR(255) NOT NULL');
     }
     
 
